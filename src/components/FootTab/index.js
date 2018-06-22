5047e29ae40352cbfc058c034e39ed21724e9b1d
Toy <1659808224@qq.com>
2018-06-22 13:53:07 +0800

    底部tab条问题

--- src/components/FootTab/index.js
+++ src/components/FootTab/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { scale } from '../../utils/dimension';
-import TouchableEx from '../TouchableEx';
+import Touchable from 'react-native-platform-touchable';
 
 export default class extends React.Component {
     renderItem = (route, index) => {
@@ -17,17 +17,18 @@ export default class extends React.Component {
             tintColor: color
         };
         return (
-            <TouchableEx
+            <Touchable
                 key={route.key}
                 onPress={() => onTabPress({route})}
+                style={{flex:1}}
             >
-                <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <View style={{alignItems: "center", justifyContent: "center",height:"100%" }}>
                     <View style={{ width: scale(29), height: scale(25), alignItems: "center", marginBottom: scale(2), marginTop: scale(2) }}>
                         {this.props.renderIcon(TabScene)}
                     </View>
                     <Text style={{ color, fontSize: scale(10), textAlign: "center" }}>{this.props.getLabelText(TabScene)}</Text>
                 </View>
-            </TouchableEx>
+            </Touchable>
         );
     };
     render() {
@@ -35,7 +36,7 @@ export default class extends React.Component {
         console.log(this.props)
         const { routes } = navigation.state;
         return (
-            <View style={{ flexDirection: 'row',justifyContent:"space-around", width: "100%", height: scale(49), borderTopColor: '#b2b2b2', borderTopWidth: 0.2, backgroundColor: "rgba(250,250,250,0.90)" }}>
+            <View style={{ flexDirection: 'row', width: "100%", height: scale(49), borderTopColor: '#b2b2b2', borderTopWidth: 0.2, backgroundColor: "rgba(250,250,250,0.90)" }}>
                 {routes && routes.map((route, index) => this.renderItem(route, index))}
             </View>
         );
