f9b281e6d180ea77c2e40e767b24d7ec4d31eaec
Toy <1659808224@qq.com>
2018-07-09 00:42:15 +0800

    购物车逻辑

--- src/page/Cart/index.js
+++ src/page/Cart/index.js
@@ -1,18 +1,20 @@
+import { observer } from "mobx-react";
 import React from 'react';
-import { Image, ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Alert, Image, ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
 import Touchable from 'react-native-platform-touchable';
+import Toast from 'react-native-root-toast';
 import Checkbox from '../../components/Checkbox';
 import NumberPicker from '../../components/NumberPicker';
 import { scale } from '../../utils/dimension';
-import UserStore from '../../utils/user'
-import {observer} from "mobx-react"
+import request from '../../utils/request';
+import UserStore from '../../utils/user';
 
 @observer
 export default class CartPage extends React.Component {
 	static navigationOptions = {
 		title: '购物车',
 		tabBarIcon: ({ focused }) => (
-			<Image style={{marginRight:3}} source={focused ? require('../../images/cart_select_icon.png') : require('../../images/cart_icon.png')} />
+			<Image style={{ marginRight: 3 }} source={focused ? require('../../images/cart_select_icon.png') : require('../../images/cart_icon.png')} />
 		),
 	}
 	state = {
@@ -29,9 +31,9 @@ export default class CartPage extends React.Component {
 	}
 	async fetchList() {
 		var user = UserStore.user
-		if(!user){
-            return
-        }
+		if (!user) {
+			return
+		}
 		var res = await fetch("https://www.bjzntq.com:8888/Commodity/getCartCommodity/", {
 			method: "POST",
 			body: JSON.stringify({
@@ -49,7 +51,7 @@ export default class CartPage extends React.Component {
 		})
 	}
 	isSelectAll(n) {
-		if(!this.state.list[n].goodsList.length){
+		if (!this.state.list[n].goodsList.length) {
 			return false
 		}
 		for (let i = 0; i < this.state.list[n].goodsList.length; i++) {
@@ -67,7 +69,7 @@ export default class CartPage extends React.Component {
 		this.setState({ selects })
 	}
 	isSelectGroupAll() {
-		if(!this.state.list.length){
+		if (!this.state.list.length) {
 			return false
 		}
 		for (let j = 0; j < this.state.list.length; j++) {
@@ -91,85 +93,110 @@ export default class CartPage extends React.Component {
 		this.state.list[i].goodsList[ii].count = val
 		this.setState({ list })
 	}
-	getSelectList(){
+	getSelectList() {
 		var selects = this.state.selects
 		var list = []
 		for (let j = 0; j < this.state.list.length; j++) {
-			let goodsList=[]
+			let goodsList = []
 			for (let i = 0; i < this.state.list[j].goodsList.length; i++) {
-				if(selects[this.state.list[j].goodsList[i].commodityId]){
+				if (selects[this.state.list[j].goodsList[i].commodityId]) {
 					list.push(this.state.list[j].goodsList[i])
 				}
 			}
 		}
 		return list
 	}
-	getSelectPrice(){
-		var list=this.getSelectList()
-		return list.map(it=>(it.deductPrice||0)*it.count).reduce(((a,b)=>a+b),0)
+	getSelectPrice() {
+		var list = this.getSelectList()
+		return list.map(it => (it.deductPrice || 0) * it.count).reduce(((a, b) => a + b), 0)
 	}
-	submit(){
+	submit() {
 		var selects = this.state.selects
 		var list = []
 		for (let j = 0; j < this.state.list.length; j++) {
-			let goodsList=[]
+			let goodsList = []
 			for (let i = 0; i < this.state.list[j].goodsList.length; i++) {
-				if(selects[this.state.list[j].goodsList[i].commodityId]){
+				if (selects[this.state.list[j].goodsList[i].commodityId]) {
 					goodsList.push(this.state.list[j].goodsList[i])
 				}
 			}
-			if(goodsList.length){
+			if (goodsList.length) {
 				list.push({
 					...this.state.list[j],
 					goodsList
 				})
 			}
 		}
-		if(!list.length){
+		if (!list.length) {
+			Toast.show("请选择商品", {
+				position: Toast.positions.CENTER
+			})
 			return
 		}
-		this.props.navigation.navigate('OrderSubmit', { list})
+		this.props.navigation.navigate('OrderSubmit', { list })
+	}
+	onRemoveItem(id) {
+		Alert.alert("确认删除?", null, [
+			{ text: "取消" },
+			{
+				text: "确定", onPress: () => {
+					this.removeItem(id)
+				}
+			}
+		])
+	}
+	async removeItem(id) {
+		var res = await request('https://www.bjzntq.com:8888/Commodity/deleteCartCommodity/', {
+			"tokeninfo": UserStore.user.tokeninfo,
+			"cart_id": id
+		})
+		this.fetchList()
+		Toast.show(res.message, {
+			position: Toast.positions.CENTER
+		})
 	}
 	render() {
 		return (
 			<View style={{ backgroundColor: '#f1f1f1', height: "100%" }}>
 				<ScrollView style={{ flex: 1, margin: scale(5) }}>
 					{this.state.list.map((it, i) => (
-						<View key={i} style={{ backgroundColor: "#fff",marginBottom:scale(5) }}>
-							<View style={{ flexDirection: "row", alignItems: "center", height: scale(39),paddingLeft:scale(10) }}>
+						<View key={i} style={{ backgroundColor: "#fff", marginBottom: scale(5) }}>
+							<View style={{ flexDirection: "row", alignItems: "center", height: scale(39), paddingLeft: scale(10) }}>
 								<Checkbox value={this.isSelectAll(i)} onChange={v => this.onSelectAll(i, v)} />
-								<Text style={{marginLeft:scale(8)}}>{it.shopName}</Text>
+								<Text style={{ marginLeft: scale(8) }}>{it.shopName}</Text>
 							</View>
 							{it.goodsList.map((itit, ii) => (
-								<View key={ii} style={{ flexDirection: "row", alignItems: "center", borderTopColor: "#ECECEC", borderTopWidth: 1,padding:scale(10) }}>
-									<Checkbox value={!!this.state.selects[itit.commodityId]} onChange={v => this.onSelect(itit.commodityId, v)} />
-									<Image source={{ uri: itit.thumb }} style={{ width: scale(80), height: scale(80),marginLeft:scale(10),marginRight:scale(8) }} />
-									<View style={{height:"100%",flex:1}}>
-										<Text numLines={2} style={{fontSize:scale(12),color:'#6A617A',height:scale(32),width:scale(145)}}>{itit.smallText}</Text>
-										<Text style={{fontSize:scale(11),color:'#989898',marginTop:1,flex:1}}>规格: {itit.specificationsValue}</Text>
-										<NumberPicker value={itit.count} onChange={v => this.setCount(i, ii, v)} min={itit.min_count} max={itit.max_count} />
+								<TouchableWithoutFeedback key={ii} onPress={() => this.props.navigation.push('Detail', { id: itit.commodityId })}>
+									<View style={{ flexDirection: "row", alignItems: "center", borderTopColor: "#ECECEC", borderTopWidth: 1, padding: scale(10) }}>
+										<Checkbox value={!!this.state.selects[itit.commodityId]} onChange={v => this.onSelect(itit.commodityId, v)} />
+										<Image source={{ uri: itit.thumb }} style={{ width: scale(80), height: scale(80), marginLeft: scale(10), marginRight: scale(8) }} />
+										<View style={{ height: "100%", flex: 1 }}>
+											<Text numLines={2} style={{ fontSize: scale(12), color: '#6A617A', height: scale(32), width: scale(145) }}>{itit.smallText}</Text>
+											<Text style={{ fontSize: scale(11), color: '#989898', marginTop: 1, flex: 1 }}>规格: {itit.specificationsValue}</Text>
+											<NumberPicker value={itit.count} onChange={v => this.setCount(i, ii, v)} min={itit.min_count} max={itit.max_count} />
+										</View>
+										<View style={{ height: "100%", alignItems: "flex-end", justifyContent: "flex-start" }}>
+											<Text style={{ fontSize: scale(12), color: "#E339D3" }}>
+												<Text>¥ </Text>
+												<Text style={{ fontSize: scale(15) }}>{itit.deductPrice}</Text>
+											</Text>
+											<Text style={{ fontSize: scale(11), color: "#A1A1A1", flex: 1, textDecorationLine: "line-through" }}>¥ {itit.price}</Text>
+											<TouchableWithoutFeedback onPress={() => this.onRemoveItem(itit.card_id)}>
+												<Image source={require("../../images/delete_icon.png")} />
+											</TouchableWithoutFeedback>
+										</View>
 									</View>
-									<View style={{height:"100%",alignItems:"flex-end",justifyContent:"flex-start"}}>
-										<Text style={{fontSize:scale(12),color:"#E339D3"}}>
-											<Text>¥ </Text>
-											<Text style={{fontSize:scale(15)}}>{itit.deductPrice}</Text>
-										</Text>
-										<Text style={{fontSize:scale(11),color:"#A1A1A1",flex:1,textDecorationLine: "line-through"}}>¥ {itit.price}</Text>
-										<TouchableWithoutFeedback>
-											<Image source={require("../../images/delete_icon.png")} />
-										</TouchableWithoutFeedback>
-									</View>
-								</View>
+								</TouchableWithoutFeedback>
 							))}
 						</View>
 					))}
 				</ScrollView>
-				<View style={{ flexDirection: "row",alignItems:"center",height:scale(60),backgroundColor:"#fff",padding:scale(10) }}>
+				<View style={{ flexDirection: "row", alignItems: "center", height: scale(60), backgroundColor: "#fff", padding: scale(10) }}>
 					<Checkbox value={this.isSelectGroupAll()} onChange={v => this.onSelectGroupAll(v)} />
-					<Text style={{fontSize:scale(15),color:'#6A617A',marginLeft:scale(11),flex:1}}>全选 ({this.getSelectList().length})</Text>
-					<Text style={{fontSize:scale(17),color:'#E339D3',marginRight:scale(17)}}>¥ {this.getSelectPrice()}</Text>
-					<Touchable onPress={this.submit.bind(this)} style={{justifyContent:"center",alignItems:"center",width:scale(68),height:scale(31),backgroundColor:"#781efd",borderRadius:scale(4)}}>
-						<Text style={{color:"#fff"}}>结算</Text>
+					<Text style={{ fontSize: scale(15), color: '#6A617A', marginLeft: scale(11), flex: 1 }}>全选 ({this.getSelectList().length})</Text>
+					<Text style={{ fontSize: scale(17), color: '#E339D3', marginRight: scale(17) }}>¥ {this.getSelectPrice()}</Text>
+					<Touchable onPress={this.submit.bind(this)} style={{ justifyContent: "center", alignItems: "center", width: scale(68), height: scale(31), backgroundColor: "#781efd", borderRadius: scale(4) }}>
+						<Text style={{ color: "#fff" }}>结算</Text>
 					</Touchable>
 				</View>
 			</View>
