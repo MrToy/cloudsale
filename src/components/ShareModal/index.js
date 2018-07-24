77402464fb6b2b8dddb66d4a03d5844830acc26c
Toy <1659808224@qq.com>
2018-07-24 14:32:11 +0800

    分享问题

--- src/components/ShareModal/index.js
+++ src/components/ShareModal/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Image, Text, View, StyleSheet } from 'react-native';
+import { Image, Text, View, StyleSheet ,Clipboard} from 'react-native';
 import Touchable from 'react-native-platform-touchable';
 import { scale } from '../../utils/dimension';
 import BottomModal from '../BottomModal';
+import Toast from 'react-native-root-toast';
 import * as WeChat from 'react-native-wechat';
 
 const styles = StyleSheet.create({
@@ -16,11 +17,28 @@ const styles = StyleSheet.create({
 
 export default class PayModal extends React.Component {
     onShare(type) {
-        WeChat.shareToSession({
-            type: 'news', 
-            description: 'hello, wechat',
-            webpageUrl:"http://www.baidu.com"
-        })
+        var shareData=this.props.data
+        if(!shareData){
+            return
+        }
+        switch(type){
+            case 1:{
+                WeChat.shareToSession(shareData)
+                break;
+            }
+            case 2:{
+                WeChat.shareToTimeline(shareData)
+                break;
+            }
+            case 3:{
+                Clipboard.setString(shareData.webpageUrl)
+                Toast.show("复制成功～", {
+                    position: Toast.positions.CENTER
+                })
+                break;
+            }
+        }
+        
         // WeChat.shareToTimeline
     }
     render() {
@@ -43,7 +61,7 @@ export default class PayModal extends React.Component {
                             <Text style={styles.iconText}>微信朋友圈</Text>
                         </View>
                     </Touchable>
-                    <Touchable onPress={() => this.onShare(2)} style={styles.touchBox}>
+                    <Touchable onPress={() => this.onShare(3)} style={styles.touchBox}>
                         <View>
                             <Image source={require('../../images/copy-link.png')} />
                             <Text style={styles.iconText}>复制链接</Text>
